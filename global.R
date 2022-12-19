24547d473198687aec0d5614ab1e8fca0ef3039f
LefMel <emeletis@outlook.com>
2022-12-19 21:19:18 +0200

    Create global.R

---
+++ global.R
@@ -0,0 +1,29 @@
+library(googledrive)
+library(XML)
+library(rlist)
+drive_deauth()
+temp <- tempfile(tmpdir = getwd(), fileext = ".zip")
+dl <- drive_download(
+  as_id("1gdWDhELYMU0lmCQz3po3Ydb7Eb1gF7j_NCs_xluCXQI"), path = temp, overwrite = TRUE)
+out <- unzip(temp, exdir = tempdir())
+
+link = paste("file:///",out[1], sep="")
+
+# Works 
+tables <- readHTMLTable(link)
+tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+data = data.frame(tables[1])
+data = data[-1,-1]
+headers = c("Date",	"Respiratory Infections", "Respiratory Infections_Cum",
+            "Pneumonia", "Pneumonia_Cum", "Flu", "Flu_Cum",
+            "Attendance at Emergency Department",
+            "COVID-19 Cases",	"COVID-19 Cases_Cum",	"Total","Total_Cum")
+colnames(data) = headers
+#n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+
+data = data[-1,1:12]
+
+
+EVI_Res_Inf = deviant(as.numeric(data$`Respiratory Infections`), r_a = 14)
+EVI_Pneum = deviant(as.numeric(data$Pneumonia, r_a=14))
+EVI_COVID = deviant(as.numeric(data$`COVID-19 Cases`[62:length(data$`COVID-19 Cases`)]), r_a=14)
