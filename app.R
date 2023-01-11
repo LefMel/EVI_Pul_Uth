d573a2565802c584039fbcb3d3d02ae629d1a7d0
LefMel <emeletis@outlook.com>
2023-01-11 08:40:21 +0200

    version10
    aes_string for PPV to fix app error

--- app.R
+++ app.R
@@ -6,7 +6,8 @@
 
 library(shiny)    
 library(shinydashboard)
-library(EVI)
+# library(EVI)
+library(ggplot2)
 
 ui <- dashboardPage(
   
@@ -146,7 +147,51 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   
-  library(EVI)
+ evi.graphs <- function (EVI_output, graph = c("EVI"), ln = TRUE, type = "p") 
+ {
+   if (!exists("EVI_output")) 
+     stop("Please run the deviant function first")
+   EVI_output$cases_1 = as.numeric(EVI_output$Cases * EVI_output$Index)
+   EVI_output$cases_1[EVI_output$cases_1 == 0] <- NA
+   EVI_output$cases_0 = as.numeric(EVI_output$Cases * (1 - EVI_output$Index))
+   EVI_output$cases_0[EVI_output$cases_0 == 0] <- NA
+   EVI_output$npv = as.numeric(EVI_output$npv * (1 - EVI_output$Index))
+   EVI_output$npv[EVI_output$npv == 0] <- NA
+   EVI_output$ppv = as.numeric(EVI_output$ppv * EVI_output$Index)
+   EVI_output$ppv[EVI_output$ppv == 0] <- NA
+   EVI_output$variable <- "x"
+   if (graph == "EVI" && ln == FALSE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = ("Cases"), 
+                           color = "Index>0"), size = 0.5), scale_color_manual(values = c("grey69", "red3")), theme(legend.position = "none"), 
+                           labs(y = "Cases", x = "Days"), if (type =="l") geom_path(aes_string(y = "Cases",colour = "factor(Index>0)")))
+   }
+   if (graph == "EVI" && ln == TRUE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = "log(Cases)", 
+                           color = "Index>0"), size = 0.5), scale_color_manual(values = c("grey69", "red3")), theme(legend.position = "none"), 
+                           labs(y = "ln(Cases)", x = "Days"), if (type =="l") geom_path(aes_string(y = "log(Cases)", colour = "factor(Index>0)")))
+   }
+   if (graph == "PPV" && ln == FALSE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = "(cases_1)", col = "ppv"), size = 0.5), geom_point(aes_string(y = "(cases_0)"), 
+                           col = "grey69", size = 0.5), labs(y = "Cases", x = ""), scale_color_gradient(low = "green", high = "red", limits = c(0, 1)), labs(color = "PPV"), 
+                           theme(legend.position = c(0.95, 0.3), legend.title = element_text(size = 10), legend.text = element_text(size = 8), legend.key.height = unit(0.5, "cm")))
+   }
+   if (graph == "PPV" && ln == TRUE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = "log(cases_1)", col = "ppv"), size = 0.5), geom_point(aes_string(y = "log(cases_0)"), 
+                           col = "grey69", size = 0.5), labs(y = "ln(Cases)", x = ""), scale_color_gradient(low = "green", high = "red", limits = c(0, 1)), labs(color = "PPV"), 
+                           theme(legend.position = c(0.95, 0.3), legend.title = element_text(size = 10), legend.text = element_text(size = 8), legend.key.height = unit(0.5,"cm")))
+   }
+   if (graph == "NPV" && ln == FALSE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = "(cases_0)", col = "npv"), size = 0.5), geom_point(aes_string(y = "(cases_1)"), 
+                           col = "grey69", size = 0.5), labs(y = "Cases"), scale_color_gradient(low = "green", high = "red", limits = c(0, 1)), labs(color = "NPV"), 
+                           theme(legend.position = c(0.95, 0.3), legend.title = element_text(size = 10), legend.text = element_text(size = 8), legend.key.height = unit(0.5, "cm")))
+   }
+   if (graph == "NPV" && ln == TRUE) {
+     sp3 <- ggplot(EVI_output, aes_string(x = "Days", group = "variable")) + list(geom_point(aes_string(y = "log(cases_0)", col = "npv"), size = 0.5), geom_point(aes_string(y = "log(cases_1)"), 
+                           col = "grey69", size = 0.5), labs(y = "ln(Cases)"), scale_color_gradient(low = "green", high = "red", limits = c(0, 1)), labs(color = "NPV"), 
+                           theme(legend.position = c(0.95, 0.3), legend.title = element_text(size = 10), legend.text = element_text(size = 8), legend.key.height = unit(0.5, "cm")))
+   }
+   print(sp3)
+ }
   # Respiratory 
   output$box1 <- renderPlot({
     evi.graphs(EVI_output=EVI_Res_Inf, graph="EVI", ln=F)
