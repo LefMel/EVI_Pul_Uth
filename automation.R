9501f41af99521b2303b63eb503f08cd449394a4
LefMel <emeletis@outlook.com>
2023-09-05 18:08:47 +0300

    version 1.3.

--- automation.R
+++ automation.R
@@ -1,5 +1,5 @@
 setwd("/srv/shiny-server/version10")
-setwd("C:/Users/LefMel/Documents/GitHub/EVI_Pul_Uth")
+#setwd("C:/Users/LefMel/Documents/GitHub/EVI_Pul_Uth")
 library(EVI)
 library(googledrive)
 library(XML)
